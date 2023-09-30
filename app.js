ebb77e63b5bc7828537c91fcf43670f8f3b10b39
AkashBharti97 <akashbharti426@gmail.com>
2023-09-30 19:19:13 +0530

    Add existing file

--- app.js
+++ app.js
@@ -13,15 +13,20 @@ root.render(heading);
 
 // =================Create a nested Html======================
 /* <div id:parent><div id:child><h1>Hello</h1></div></div> */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
+const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "Hello H1 tag "),
-    React.createElement("h2", {}, "Hello H2 tag "),
-  ])
-);
+    React.createElement("h1", {}, "Im a h1 tag"),
+    React.createElement("h2", {}, "Im a h2 tag"),
+  ]),
+
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "Im a h1 tag"),
+    React.createElement("h2", {}, "Im a h2 tag"),
+  ]),
+]);
 
 console.log(parent);
 
